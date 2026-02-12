6197d205ca21a5bce9ab8ed1793c7e2fc4a76033
akbarazy <muh.akbar.alfarizy@gmail.com>
2026-02-12 17:13:28 +0700

    Include source files

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import '@fontsource/heebo/400.css';
+import '@fontsource/heebo/500.css';
+import '@fontsource/heebo/600.css';
+import '@fontsource/nunito/600.css';
+import '@fontsource/nunito/700.css';
+import '@fontsource/nunito/800.css';
+import '@fontsource/pacifico';
+import './bootstrap.min.css';
+import './index.css';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
