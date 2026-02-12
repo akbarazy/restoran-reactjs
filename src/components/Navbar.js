3ac975df44008b9e82c90c330fd6e693b703d460
akbarazy <muh.akbar.alfarizy@gmail.com>
2026-02-12 17:56:38 +0700

    Create hero and update navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,70 +1,70 @@
-import { Link, NavLink } from "react-router-dom";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faUtensils, faBars } from "@fortawesome/free-solid-svg-icons";
+import { Link, NavLink } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUtensils, faBars } from '@fortawesome/free-solid-svg-icons';
 
 function Navbar() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark px-4 px-lg-5 py-3 py-lg-0">
-      <Link to="/" className="navbar-brand p-0">
-        <h1 className="text-primary m-0">
-          <FontAwesomeIcon icon={faUtensils} className="me-2" />Restoran
+    <nav className='navbar navbar-expand-lg navbar-dark bg-dark px-4 px-lg-5 py-3 py-lg-0'>
+      <Link to='/' className='navbar-brand p-0'>
+        <h1 className='text-primary m-0'>
+          <FontAwesomeIcon icon={faUtensils} className='me-2' />Restoran
         </h1>
       </Link>
 
       <button
-        className="navbar-toggler"
-        type="button"
-        data-bs-toggle="collapse"
-        data-bs-target="#navbarCollapse"
-        aria-controls="navbarCollapse"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
+        className='navbar-toggler'
+        type='button'
+        data-bs-toggle='collapse'
+        data-bs-target='#navbarCollapse'
+        aria-controls='navbarCollapse'
+        aria-expanded='false'
+        aria-label='Toggle navigation'
       >
         <FontAwesomeIcon icon={faBars} />
       </button>
 
-      <div className="collapse navbar-collapse" id="navbarCollapse">
-        <div className="navbar-nav ms-auto py-0 pe-4">
-          <NavLink to="/" className="nav-item nav-link">
+      <div className='collapse navbar-collapse' id='navbarCollapse'>
+        <div className='navbar-nav ms-auto py-0 pe-4'>
+          <NavLink to='/' className='nav-item nav-link'>
             Home
           </NavLink>
-          <NavLink to="/about" className="nav-item nav-link">
+          <NavLink to='/about' className='nav-item nav-link'>
             About
           </NavLink>
-          <NavLink to="/service" className="nav-item nav-link">
+          <NavLink to='/service' className='nav-item nav-link'>
             Service
           </NavLink>
-          <NavLink to="/menu" className="nav-item nav-link">
+          <NavLink to='/menu' className='nav-item nav-link'>
             Menu
           </NavLink>
 
-          <div className="nav-item dropdown">
+          <div className='nav-item dropdown'>
             <span
-              className="nav-link dropdown-toggle"
-              role="button"
-              data-bs-toggle="dropdown"
+              className='nav-link dropdown-toggle'
+              role='button'
+              data-bs-toggle='dropdown'
             >
               Pages
             </span>
-            <div className="dropdown-menu m-0">
-              <NavLink to="/booking" className="dropdown-item">
+            <div className='dropdown-menu m-0'>
+              <NavLink to='/booking' className='dropdown-item'>
                 Booking
               </NavLink>
-              <NavLink to="/team" className="dropdown-item">
+              <NavLink to='/team' className='dropdown-item'>
                 Our Team
               </NavLink>
-              <NavLink to="/testimonial" className="dropdown-item">
+              <NavLink to='/testimonial' className='dropdown-item'>
                 Testimonial
               </NavLink>
             </div>
           </div>
 
-          <NavLink to="/contact" className="nav-item nav-link">
+          <NavLink to='/contact' className='nav-item nav-link'>
             Contact
           </NavLink>
         </div>
 
-        <Link to="/booking" className="btn btn-primary py-2 px-4">
+        <Link to='/booking' className='btn btn-primary py-2 px-4'>
           Book A Table
         </Link>
       </div>
--- src/components/shared/Header.js
+++ src/components/shared/Header.js
@@ -1,9 +1,11 @@
-import Navbar from "../Navbar";
+import Hero from '../Hero';
+import Navbar from '../Navbar';
 
 function Header() {
   return (
     <div className='container-xxl position-relative p-0'>
       <Navbar />
+      <Hero />
     </div>
   );
 }
