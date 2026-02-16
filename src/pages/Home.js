6a256af4b53606b78728b7c0e29ca2055115ef62
akbarazy <muh.akbar.alfarizy@gmail.com>
2026-02-16 16:56:53 +0700

    Create team component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,6 +2,7 @@ import Service from "./Service";
 import About from "./About";
 import Menu from "./Menu";
 import Booking from "./Booking";
+import Team from "./Team";
 
 function Home() {
   return (
@@ -10,6 +11,7 @@ function Home() {
       <About />
       <Menu />
       <Booking />
+      <Team />
     </>
   );
 }
---
+++ src/pages/Team.js
@@ -0,0 +1,53 @@
+import { Link } from 'react-router-dom';
+import teamImg1 from '../assets/images/team-1.jpg';
+import teamImg2 from '../assets/images/team-2.jpg';
+import teamImg3 from '../assets/images/team-3.jpg';
+import teamImg4 from '../assets/images/team-4.jpg';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebookF, faInstagram, faTwitter } from '@fortawesome/free-brands-svg-icons';
+
+const teamMembers = [
+  { name: "Full Name", designation: "Designation", img: teamImg1 },
+  { name: "Full Name", designation: "Designation", img: teamImg2 },
+  { name: "Full Name", designation: "Designation", img: teamImg3 },
+  { name: "Full Name", designation: "Designation", img: teamImg4 },
+];
+
+function Team() {
+  return (
+    <div className="container-xxl pt-5 pb-3">
+      <div className="container">
+        <div className="text-center mb-5">
+          <h5 className="section-title ff-secondary text-center text-primary fw-normal">Team Members</h5>
+          <h1>Our Master Chefs</h1>
+        </div>
+        <div className="row">
+          {teamMembers.map((member, index) => (
+            <Card key={index} member={member} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function Card({ member }) {
+  return (
+    <div className="col-lg-3 col-md-6 mb-4">
+      <div className="team-item text-center rounded overflow-hidden">
+        <div className="rounded-circle overflow-hidden m-4">
+          <img className="img-fluid" src={member.img} alt={member.name} />
+        </div>
+        <h5 className="mb-0">{member.name}</h5>
+        <small>{member.designation}</small>
+        <div className="d-flex justify-content-center mt-3">
+          <Link className="btn btn-square btn-primary mx-1" to="#"><FontAwesomeIcon icon={faFacebookF} /></Link>
+          <Link className="btn btn-square btn-primary mx-1" to="#"><FontAwesomeIcon icon={faTwitter} /></Link>
+          <Link className="btn btn-square btn-primary mx-1" to="#"><FontAwesomeIcon icon={faInstagram} /></Link>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Team;
